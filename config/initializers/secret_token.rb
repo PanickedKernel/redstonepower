a435af6ae612a774c0c661c90a6ede71765396dc
PanickedKernel <npzr101@gmail.com>
2013-02-17 01:40:37 +0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Redstonepower::Application.config.secret_token = 'a9e0455f795cac6c2eca074508ea6fd92f6e6cb727ea77fbc0d383fce1fb606d19d3519ead948e8897327cdce91705760a3e92b17ff715944fa8c4f02e7f464c'
