ca8b5e6a1a1559bd4d42234079755ab3cfd86d6d
Noah <npzr101@gmail.com>
2013-04-04 15:34:47 -0400

    improved compatability between rails_admin and roles, tweaked avatar to fetch from minotar.net API

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,5 +2,10 @@ class Role < ActiveRecord::Base
   has_and_belongs_to_many :users, :join_table => :users_roles
   belongs_to :resource, :polymorphic => true
   
+  rails_admin do 
+  configure :resource, :polymorphic_association         # Hidden
+  configure :users, :has_and_belongs_to_many_association
+  end
+
   scopify
 end
